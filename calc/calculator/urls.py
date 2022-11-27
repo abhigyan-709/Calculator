fde05f446d931991f7cb283745627ed05a88c4d5
Abhigyan Kumar <abhigkumar709@gmail.com>
2022-11-28 02:14:40 +0530

    first commit

---
+++ calc/calculator/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('add', views.addition, name='add'),
+    path('sub', views.subtraction, name='sub'),
+    path('multi', views.multiplication, name='multi'),
+    path('div', views.division, name='div')
+]
+
